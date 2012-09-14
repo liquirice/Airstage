695bed6915be4f1ba04ad027ed37f4320a72f4bb
Kam Zhong He <two_saki@hotmail.com>
2012-09-15 01:29:53 +0800

    修改活動分享權限

--- page/airstab/app/apply/private.php
+++ page/airstab/app/apply/private.php
@@ -1,14 +1,36 @@
 <?php
 echo'
 <div id="private">
+<form action="apply02.php?action=private" method="post">
 <table align="center" background="jpg/">
 	<tr>
-		<td align="center"><h1 align="center">此功能尚未開放,敬請期待!</h1></td>
+		<td align="center">請輸入欲共同管理此活動者的學號</td>
+	</tr>
+	<tr>
+		<td><input type="text" name="private" /><br /><span style="color:#FF0000; font-size:12px">請將各個學號以","分開</span></td>
+	</tr>
+	<tr>
+		<td><input type="submit" value="提交" /></td>
 	</tr>
 	<tr>
 		<td align="center"><input type="button" class="back" value="返回上頁" /></td>
 	</tr>
 </table>
+</form>
 </div>
 ';
+if($_SESSION['action'] == 'private'){
+	$prirno = $url['rno'];
+	$private = $_POST['private'];
+	$old = $url['stu_id'];
+	$new = ''.$old.','.$private.'';
+	$updatepri = "UPDATE Activities SET stu_id = '$new' WHERE rno = $prirno";
+	
+	if(mysqli_query($conn, $updatepri)){
+		echo '<script type="text/javascript" language="javascript">alert("設定成功!"); window.reload();</script>';
+	}
+	else{
+		echo '<script type="text/javascript" language="javascript">alert("設定失敗!請重新再來"); window.reload();</script>';
+	}
+}
 ?>
