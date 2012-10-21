a35572cd19acee0a996425424620b19a3ed3cb6c
Archerwind <archerwindy@gmail.com>
2012-10-21 22:05:26 +0800

    Just syn to the latest version

--- marketProject/sellerInterface.php
+++ marketProject/sellerInterface.php
@@ -5,11 +5,16 @@
 	if( !isset($_SESSION['stu_id']) || !isset($_SESSION['name']) || !isset($_SESSION['auth']) || !isset($_SESSION['nick']) ) {
 		echo '<script type="text/javascript">alert("請先登入!"); location.href="marketIndex.php"</script>';
 	} else {
-		/*require_once( "../connectVar.php" );
+		require_once( "../connectVar.php" );
 		$stu_id = $_SESSION['stu_id'];
 		
-		$query = "SELECT * FROM chasing_list INNER JOIN marketContention_trade Using(stu_id) WHERE chasing_list.stu_id = '$stu_id'";
-		$result = mysqli_query( $conn, $query );*/
+		$query = "SELECT COUNT(markTime), * FROM Member " .
+				 "LEFT JOIN marketSecondHand_trade ON Member.stu_id = marketSecondHand_trade.stu_id " .
+				 "LEFT JOIN marketSecondHand_bidList ON marketSecondHand_trade.trade_id = marketSecondHand_bidList.trade_id " .
+				 "LEFT JOIN marketSecondHand_time ON marketSecondHand_trade.trade_id = marketSecondHand_time.trade_id " .
+				 "LEFT JOIN marketSecondHand_chasingList ON marketSecondHand_trade.trade_id = marketSecondHand_chasingList.trade_id " .
+				 "WHERE Member.stu_id = '$stu_id'";
+		$resultSecondHand = mysqli_query( $conn, $query ) or die('Query Error');
 	}
 ?>
 
@@ -46,143 +51,171 @@
 	<!-- Warning Area -->
 	<div class="alert alert-info fade in">
 		<button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong>Airstage 提醒：</strong>
+        <strong>Airstage 提醒：</strong>記得用點案的方式打開各個市場的清單唷！
     </div>
-	
-	
-	<!-- Second Hand Area -->
-	<h3>二手市場</h3>
-	<table class="table table-striped">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>商品名稱 <i class="icon-chevron-down"></i></th>
-          <th>出價金額 <i class="icon-chevron-down"></i></th>
-          <th>出價人數 <i class="icon-chevron-down"></i></th>
-          <th>追蹤人數 <i class="icon-chevron-down"></i></th>
-          <th>出價時間 <i class="icon-chevron-down"></i></th>
-          <th>狀態 <i class="icon-chevron-down"></i></th>
-          <th>買賣管理</th>          
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td><a href = "#">Adobe Photoshop CS6</a></td>
-          <td>       
-	      	NT$2400          	
-          </td>
-          <td>20 人</td>
-          <td>2 人</td>
-          <td>2012 / 10 / 10</td>
-          <td><font color="red">拍賣中</font></td>
-          <td>
-          	<a href="productSellInterfaceS.php"><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
-          </td>       
-        </tr>
-        <tr>
-          <td>2</td>
-          <td><a href = "#">Adobe Photoshop CS6</a></td>
-          <td>
-	          NT$5000          
-	      </td>
-          <td>100 人</td>
-          <td>72 人</td>
-          <td>2012 / 10 / 10</td>
-          <td>已結案</td>
-          <td>
-	      	<a href=""><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    
+    <!-- Second Hand Area -->
+	<div class="accordion-group">
+      <div class="accordion-heading">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">
+          <h3>二手市場</h3>
+        </a>
+      </div>
+      <div id="collapseOne" class="accordion-body collapse in">
+        <div class="accordion-inner">
+          <table class="table table-striped">
+		      <thead>
+		        <tr>
+		          <th>#</th>
+		          <th>商品名稱 <i class="icon-chevron-down"></i></th>
+		          <th>出價金額 <i class="icon-chevron-down"></i></th>
+		          <th>出價人數 <i class="icon-chevron-down"></i></th>
+		          <th>追蹤人數 <i class="icon-chevron-down"></i></th>
+		          <th>出價時間 <i class="icon-chevron-down"></i></th>
+		          <th>狀態 <i class="icon-chevron-down"></i></th>
+		          <th>買賣管理</th>       
+		        </tr>
+		      </thead>
+		      <tbody>
+		        <tr>
+		          <td>1</td>
+		          <td><a href = "#">Adobe Photoshop CS6</a></td>
+		          <td>       
+			      	NT$2400          	
+		          </td>
+		          <td>20 人</td>
+		          <td>2 人</td>
+		          <td>2012 / 10 / 10</td>
+		          <td><font color="red">拍賣中</font></td>
+		          <td>
+		          	<a href="productSellInterfaceS.php"><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
+		          </td>       
+		        </tr>
+		        <tr>
+		          <td>2</td>
+		          <td><a href = "#">Adobe Photoshop CS6</a></td>
+		          <td>
+			          NT$5000          
+			      </td>
+		          <td>100 人</td>
+		          <td>72 人</td>
+		          <td>2012 / 10 / 10</td>
+		          <td>已結案</td>
+		          <td>
+			      	<a href=""><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>			   
+		          </td>
+		        </tr>
+		      </tbody>
+		   </table>
+        </div>
+      </div>
+    </div>
     
     
     <hr class="bs-docs-separator">
-    <!-- Contention Area -->
-    <h3>限時競標</h3>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>商品名稱 <i class="icon-chevron-down"></i></th>
-          <th>目前價格 <i class="icon-chevron-down"></i></th>
-          <th>出價人數 <i class="icon-chevron-down"></i></th>
-          <th>追蹤人數 <i class="icon-chevron-down"></i></th>
-          <th>剩餘時間 <i class="icon-chevron-down"></i></th>          
-          <th>買賣管理</th>          
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td><a href = "#">Adobe Photoshop CS6</a></td>
-          <td>NT$200000</td>
-          <td>40 人</td>
-          <td>21 人</td>
-          <td><a href="">00:00:03</a></td>          
-          <td>
-	        <a href="productSellInterfaceC.php"><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
-          </td>       
-        </tr>
-        <tr>
-          <td>2</td>
-          <td><a href = "#">Adobe Photoshop CS6</a></td>
-          <td>NT$200000</td>
-          <td>52 人</td>
-          <td>45 人</td>          
-          <td>已結標</td>
-          <td>
-	      	<a href=""><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
-          </td>
-        </tr>               
-      </tbody>
-    </table>
     
+    <div class="accordion-group">
+      <div class="accordion-heading">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo">
+          <h3>限時競標</h3>
+        </a>
+      </div>
+      <div id="collapseTwo" class="accordion-body collapse">
+        <div class="accordion-inner">
+          <table class="table table-striped">
+		      <thead>
+		        <tr>
+		          <th>#</th>
+		          <th>商品名稱 <i class="icon-chevron-down"></i></th>
+		          <th>目前價格 <i class="icon-chevron-down"></i></th>
+		          <th>出價人數 <i class="icon-chevron-down"></i></th>
+		          <th>追蹤人數 <i class="icon-chevron-down"></i></th>
+		          <th>剩餘時間 <i class="icon-chevron-down"></i></th>          
+		          <th>買賣管理</th>     
+		        </tr>
+		      </thead>
+		      <tbody>
+			  <!--tr>
+		          <td>1</td>
+		          <td><a href = "#">Adobe Photoshop CS6</a></td>
+		          <td>NT$200000</td>
+		          <td>40 人</td>
+		          <td>21 人</td>
+		          <td><a href="">00:00:03</a></td>          
+		          <td>
+			        <a href="productSellInterfaceC.php"><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
+		          </td>       
+		        </tr>
+		        <tr>
+		          <td>2</td>
+		          <td><a href = "#">Adobe Photoshop CS6</a></td>
+		          <td>NT$200000</td>
+		          <td>52 人</td>
+		          <td>45 人</td>          
+		          <td>已結標</td>
+		          <td>
+			      	<a href=""><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
+		          </td>
+		        </tr-->
+		      </tbody>
+		   </table>	    
+        </div>
+      </div>
+    </div>
     
     <hr class="bs-docs-separator">
-    <!-- Group Shop Area -->
-    <h3>校園團購</h3>
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>商品名稱 <i class="icon-chevron-down"></i></th>
-          <th>目前價格 <i class="icon-chevron-down"></i></th>
-          <th>跟團人數 <i class="icon-chevron-down"></i></th>
-          <th>追蹤人數 <i class="icon-chevron-down"></i></th>
-          <th>剩餘時間 <i class="icon-chevron-down"></i></th>
-          <th>狀態 <i class="icon-chevron-down"></i></th>
-          <th>買賣管理</th>          
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td><a href = "#">Adobe Photoshop CS6</a></td>
-          <td>NT$200000</td>
-          <td>20 人</td>
-          <td>4 人</td>
-          <td><a href="#">00:00:04</a></td>
-          <td>已結團</td>
-          <td>
-	        <a href=""><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
-          </td>       
-        </tr>
-        <tr>
-          <td>2</td>
-          <td><a href = "#">Adobe Photoshop CS6</a></td>
-          <td>NT$200000</td>
-          <td>20 人	</td>
-          <td>4 人</td>
-          <td><a href="#">00:00:04</a></td>
-          <td><font color="red">揪團中</font></td>
-          <td>
-	        <a href=""><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
-          </td>
-        </tr>                
-      </tbody>
-    </table>
+    
+    <div class="accordion-group">
+      <div class="accordion-heading">
+        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion2" href="#collapseThree">
+          <h3>校園團購</h3>
+        </a>
+      </div>
+      <div id="collapseThree" class="accordion-body collapse">
+        <div class="accordion-inner">    
+          <table class="table table-striped">
+		      <thead>
+		        <tr>
+		          <th>#</th>
+		          <th>商品名稱 <i class="icon-chevron-down"></i></th>
+		          <th>目前價格 <i class="icon-chevron-down"></i></th>
+		          <th>跟團人數 <i class="icon-chevron-down"></i></th>
+		          <th>追蹤人數 <i class="icon-chevron-down"></i></th>
+		          <th>剩餘時間 <i class="icon-chevron-down"></i></th>
+		          <th>狀態 <i class="icon-chevron-down"></i></th>
+		          <th>買賣管理</th>       
+		        </tr>
+		      </thead>
+		      <tbody>
+		        <!--tr>
+		          <td>1</td>
+		          <td><a href = "#">Adobe Photoshop CS6</a></td>
+		          <td>NT$200000</td>
+		          <td>20 人</td>
+		          <td>4 人</td>
+		          <td><a href="#">00:00:04</a></td>
+		          <td>已結團</td>
+		          <td>
+			        <a href=""><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
+		          </td>       
+		        </tr>
+		        <tr>
+		          <td>2</td>
+		          <td><a href = "#">Adobe Photoshop CS6</a></td>
+		          <td>NT$200000</td>
+		          <td>20 人	</td>
+		          <td>4 人</td>
+		          <td><a href="#">00:00:04</a></td>
+		          <td><font color="red">揪團中</font></td>
+		          <td>
+			        <a href=""><button class="btn btn-info">前往 <i class="icon-edit icon-white"></i></button></a>
+		          </td>
+		        </tr-->
+		      </tbody>
+		   </table>
+        </div>
+      </div>
+    </div>
 	
 </div>
 <!-- End Container -->
