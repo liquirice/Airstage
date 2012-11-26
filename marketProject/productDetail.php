fc18768b9f57e1f7a0a3f81adaa8536a5a7d332e
huskpurin <huskpurin@gmail.com>
2012-11-26 23:26:18 +0800

    準備商品詳細資訊頁

--- marketProject/productDetail.php
+++ marketProject/productDetail.php
@@ -4,12 +4,15 @@
 	// 
 	require_once( "../setSession.php" );
 	
-	//比對product_id & token: 查驗方法??
+	//比對product_id & token，確保安全性，但我還不知道為什麼要這麼做（看起來似乎沒啥意義）。
+	//總之確定是用 $_GET['num'] 抓trade_id
 	$trade_id = intval( htmlspecialchars( trim( $_GET['num'] ) ) );
 	$token = htmlspecialchars( trim( $_GET['token'] ) ) ;
+	
+	//判斷token是否正確
 	if( SAH1($trade_id) != $token ) {
-		echo "<script type='text/javascript'>alert('商品網址有誤，請重新輸入'); history.back();</script>";
-		$error = 1;
+		//不正確，回到上一頁
+		echo "<script type='text/javascript'>alert('商品網址有誤，請重新確認'); location.href='./secondHandIndex.php';</script>";
 		exit();
 	}
 	else {
@@ -18,19 +21,23 @@
 		$productDetail = "SELECT * FROM ( (`marketSecondHand_trade` INNER JOIN `marketSecondHand_time` USING ( time_id ) ) INNER JOIN `marketSecondHand_productInfo` USING ( product_id ) ) WHERE `marketSecondHand_trade`.`trade_id` = {$trade_id}";
 		$result = mysqli_query( $conn, $productDetail );
 		
-		if( mysqli_num_rows($result) == 0 )
-		{
-			echo "<script type='text/javascript'>alert('查無此商品，請重新輸入'); history.back();</script>";
+		//判斷商品是否存在
+		if( mysqli_num_rows($result) == 0 ) {
+			echo "<script type='text/javascript'>alert('查無此商品，請重新確認'); history.back();</script>";
 			$error = 2;
 			exit();
 		}
-		else {}
+		else {
+		}
+		
+		//判斷身分
 		if( !isset($_SESSION['stu_id']) || !isset($_SESSION['name']) || !isset($_SESSION['auth']) ) {
-		//訪客
+			//訪客
 	
 		} 
 		else {
-		//賣家或買家
+			//賣家或買家
+			//比對 $_SESSION['stu_id'] 是否等於此商品的stu_id，來看是否為賣家
 		}
 	}
 ?>
@@ -64,39 +71,21 @@
 	    require_once( "memberStateLine.php" );
 	?>
 	
-	<!--路徑欄-->
+	<!--路徑-->
 	<ul class="breadcrumb">
 		<li><a href="marketIndex.php">市場首頁</a> <span class="divider">/</span></li>
 		<!--li>子目錄 <span class="divider">/</span></li-->
 		<li class="active">當前頁面</li>
 	</ul>
+    
+    
+    
 
-	<!-- Warning Area -->
-	<div class="alert alert-danger fade in">
-		<button type="button" class="close" data-dismiss="alert">&times;</button>
-		<strong>Attention!</strong> &nbsp;請務必閱讀競標規則再進入唷！
-	</div>
-	
-	<!--每個橫段有row、row-fluid(稍擠)二種選擇-->
-	<div class="row">
-		<!-- 空間內由一至數個span*組成，*=1~12 -->
-		<!-- 數量不限，*的總合<=12，12為最佳狀態剛好填滿。以下為範例-->
-		<div class="span6"></div>
-		<div class="span6"></div>
-	</div>
-	<div class="row-fluid">
-		<div class="span2"></div>
-		<div class="span4"></div>
-		<div class="span3"></div>
-		<div class="span3"></div>
-	</div>
 </div>
 
 <!--Footer-->
 <?php
 	require_once( "marketFooter.php" );
-	
-	//$_SERVER['PHP_SELF'];
 ?>
 
 <script src = "js/bootstrap-modal.js"></script>
