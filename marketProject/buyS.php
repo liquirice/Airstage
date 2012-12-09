f602ce292c4b48ce744bcff3898fe8bb1e6e4254
huskpurin <huskpurin@gmail.com>
2012-12-09 19:03:34 +0800

    update marketProject

--- marketProject/buyS.php
+++ marketProject/buyS.php
@@ -1,50 +1,76 @@
 ﻿<?php
-	// Last Modified Day : 2012.09.23
+	// Last Modified Day : 2012.12.09
+	
+	echo "now loading...";
+
+	require_once( "../setSession.php" );
 	
 	//確定登入狀態
-	if( !isset($_SESSION['stu_id']) || !isset($_SESSION['name']) || !isset($_SESSION['auth']) || !isset($_SESSION['nick']) )
+	if( !isset($_SESSION['stu_id']) || !isset($_SESSION['name']) || !isset($_SESSION['auth']) )	{
+		echo '<script type="text/javascript">alert("請先登入唷～"); history.back();</script>';
+		exit();
+	} else if( $_SESSION['auth'] < 1 ) {
+		echo '<script type="text/javascript">alert("請先通過信箱認證，才可以進行市集的交易唷！"); history.back();"</script>';
+		exit();
+	}
+
+	//接收資料
+	$bidder_id = $_SESSION['stu_id'];
+	$trade_id = mysqli_real_escape_string( $conn, trim( $_POST['trade']) );
+	$exchange_type = mysqli_real_escape_string( $conn, trim( $_POST['pay_type']) );
+	$exchange_info = mysqli_real_escape_string( $conn, trim( $_POST['pay_info']) );
+	$wanted_number = mysqli_real_escape_string( $conn, trim( $_POST['num']) );
+	
+	//連接資料庫
+	require_once("../connectVar.php");
+	
+	//確認商品資訊
+	$query = "SELECT * FROM `marketSecondHand_trade` WHERE `trade_id` = '$trade_id'";
+	$result = mysqli_query( $conn, $query );
+	if( mysqli_num_rows($result) == 0 )	{
+		echo '<script type="text/javascript">alert("沒有這項商品唷！"); history.back();</script>';
+		exit();
+	}
+
+	$row = mysqli_fetch_array($result);
+
+	//商品已結標
+	if( $row['exist'] <= 0 ) {
+		echo '<script type="text/javascript">alert("出價失敗！商品已經下價或截標囉！"); history.back();</script>';
+		exit();
+	}
+	//出價數 > 剩餘數
+	if( $row['number'] < $wanted_number ) {
+		echo '<script type="text/javascript">alert("出價失敗！商品剩餘量不足，請重新確認下標數量。"); history.back();</script>';
+		exit();
+	}
+	
+	//確認為可出價的商品，查詢買家出價紀錄
+	$query = "SELECT * FROM `marketSecondHand_bidList` WHERE `trade_id` = '$trade_id' AND `bidder_id` = '$bidder_id'";
+	$result = mysqli_query( $conn, $query );
+	$times = mysqli_num_rows($result);
+	
+	if( $times == 0 )	
+	{	//當出價紀錄為0筆 : INSERT
+		$bid_query = "INSERT INTO `marketSecondHand_bidList` (`bidder_id`, `trade_id`, `exchange_type`, `exchange_info`, `wanted_number`, `buy_list`, `update_time`) VALUES ( '$bidder_id', '$trade_id', '$exchange_type', '$exchange_info', '$wanted_number', '0', now() );";
+	}
+	else if( $times == 1 )
+	{	//曾出過架 - 更新出價 : UPDATE
+		$bid_query = "UPDATE `marketSecondHand_bidList` SET `exchange_type` = '$exchange_type', `exchange_info` = '$exchange_info', `wanted_number` = '$wanted_number' WHERE `bidder_id` = '$bidder_id' AND `trade_id` = '$trade_id' ;";
+	}
+	else
+	{	//出現二筆以上資料 : GG
+		echo '<script type="text/javascript">alert("出價失敗！資料庫錯誤！請與管理人員聯絡。"); history.back();</script>';
+		exit();
+	}
+		
+	if( mysqli_query( $conn, $bid_query ) )
 	{
-		echo '<script type="text/javascript">alert("請先登入!"); location.href="marketIndex.php"</script>';
+		echo '<script type="text/javascript">alert("已經完成出價！"); history.back();</script>';
 	}
 	else
 	{
-		//連接資料庫
-		require_once("../connectVar.php");
-
-		session_start();
-		
-		$bidder_id = mysqli_real_escape_string( $conn, trim( $_SESSION['stu_id']) );
-		$trade_id = mysqli_real_escape_string( $conn, trim( $_POST['trade_id']) );
-		$exchange_type = mysqli_real_escape_string( $conn, trim( $_POST['exchange_type']) );
-		$exchange_info = mysqli_real_escape_string( $conn, trim( $_POST['exchange_info']) );
-		$wanted_number = mysqli_real_escape_string( $conn, trim( $_POST['wanted_number']) );
-
-		//檢查info字串長度		
-		//檢查number是否<=剩餘數量
-		
-		//僅寫入或更新bidList
-		//先用stu_id+trade_id判斷為新出價或更新出價
-		$result = mysqli_query( $conn, "SELECT * FROM `marketSecondHand_bidList` WHERE `bidder_id` = '{$bidder_id}' AND `trade_id` = '{$trade_id}'" );
-		$new_check = mysqli_fetch_assoc($result);
-		
-		//結果數量=0:新出價
-		if(1){
-			//insert: marketSecondHand_bidList
-			//bidder_id, trade_id, exchange_type, exchange_info, wanted_number, buy_list=0, update_time=NULL
-		}
-		//結果數量=1:修改出價
-		else if(1){
-			//先判斷buy_list為0或1，是否已為得標者(是否要讓已得標者能夠再次出價!?!?!?!?)
-			if(!1){}
-			else{}
-			//where : bidder_id, trade_id
-			//update: exchange_type, exchange_info, wanted_number, buy_list=0, update_time=NULL
-
-		}
-		//結果數量>1:error
-		else{
-			echo "<script type='text/javascript'>alert('系統錯誤！請回報管理人員！'); history.back();</script>";
-		}
-		
+		echo '<script type="text/javascript">alert("出價失敗！請稍後重試！"); history.back();</script>';
 	}
+
 ?>
--- marketProject/categoryS.php
+++ marketProject/categoryS.php
@@ -1,12 +1,12 @@
 ﻿<?php
-	// Last Modified Day : 2012.10.14
+	// Last Modified Day : 2012.10.01
 	// No need to login to browse the products.
 	session_start();
 ?>
 
 <!DOCTYPE HTML>
 <head>
-	<title>Airstage - Student Market - Contention</title>
+	<title>Airstage - Student Market - SecondBook</title>
 	<link href = "../tm2.ico" rel = "shortcut icon" />
 	<link href = "css/bootstrap.css" rel = "stylesheet" />
 	<link href = "css/baseCss.css" rel = "stylesheet" />
@@ -14,23 +14,33 @@
 	<meta http-equiv = "Content-Type" content = "text/html; charset = utf8" />
 	<meta http-equiv = "Content-Language" content = "zh-tw" />
 	<style>
-		.nav h4{
-			text-align:top;
-			line-height:30px; 
+		.span2 {
+			text-align:right;
+			vertical-align:center;
+			/*background-color:;*/
 		}
-		.well li{
-			list-style-image:url('sqpurple.gif');
-			line-height:28px;
+		#myTabContent {
+			border-left-color:#ff0000;
 		}
-		ul{
-			list-style-type:none;
+		ul.vertical_list {
+			display:inline;
+			list-style:inside;
+	
 		}
-		/*每個主分類在最後多一條底線
-		.tab-content{
-			padding-bottom: 9px; border-bottom: 1px solid #ddd;
+		ul.vertical_list li {
+			float:left;
+			margin-left:5px;
+			margin-bottom:15px;
+			width:140px;
 		}
-		*/
+		.tab-content {
+			padding-left:20px;
+			padding-right:0px;
+		}
 	</style>
+	<script>
+	
+	</script>
 </head>
 
 <body>
@@ -42,293 +52,254 @@
 
 <!-- Container Start -->
 <div class = "container">
-	<div class="progress progress-striped active">
-			<div class="bar bar-primary" style="width: 60%;"></div>
-	</div>
 
+	<!--進度條-->
+	<div class="progress progress-striped active">
+		<div class="bar bar-primary" style="width: 5%;"></div>
+	</div>
+	
+	<!--market bar-->
     <?php
 	    require_once( "memberStateLine.php" );
 	?>
-		
-	<!--Breadcrumbs-->
+	
+	<!--路徑欄-->
 	<ul class="breadcrumb">
 		<li><a href="marketIndex.php">市場首頁</a> <span class="divider">/</span></li>
-		<li><a href="#">限時競標</a> <span class="divider">/</span></li>
-		<li class="active">全部分類</li>
+		<li><a href="contentionBoard.php">二手市場</a> <span class="divider">/</span></li>
+		<!--li><a href="contentionBoard.php">分類搜尋</a> <span class="divider">/</span></li-->
+		<li class="active">分類列表</li>
 	</ul>
 	
-	<!-- Warning Area -->
-	<div class="alert alert-danger fade in">
-        <button type="button" class="close" data-dismiss="alert">&times;</button>
-        <strong>Attention!</strong> &nbsp;請務必閱讀競標規則再進入唷！
-    </div>
+	<!--書籍雜誌-->
+	<div class="row-fluid" >
+		<div class="span2">
+			<h4><a href="searchS.php?ct=01">書籍雜誌</a></h4>
+		</div>
+		<div class="span10">
+			<ul class="nav nav-tabs" id="books">
+				<li class="active"><a data-toggle="tab" href="#textbook">課程用書</a></li>
+				<li><a data-toggle="tab" href="#book">一般書籍</a></li>
+				<li><a data-toggle="tab" href="#magazine">報刊雜誌</a></li>
+				<li><a data-toggle="tab" href="#foreign_book">外文書籍</a></li>
+			</ul>
+			<div class="tab-content" id="booksSubCat">
+				<div id="textbook" class="tab-pane fade in active">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0100010001">文學院</a></li>
+							<li><a href="searchS.php?ct=0100010002">理學院</a></li>
+							<li><a href="searchS.php?ct=0100010003">工學院</a></li>
+							<li><a href="searchS.php?ct=0100010004">管理學院</a></li>
+							<li><a href="searchS.php?ct=0100010005">海洋科學院</a></li>
+							<li><a href="searchS.php?ct=0100010006">社會科學院</a></li>
+							<li><a href="searchS.php?ct=0100010007">通識課程</a></li>
+							<li><a href="searchS.php?ct=0100010008">其他課程</a></li>
+						</ul>
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=010001">所有課程用書</a>
+					</div>
+				</div>
+				<div id="book" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0100020001">商業理財</a></li>
+							<li><a href="searchS.php?ct=0100020002">文學小說</a></li>
+							<li><a href="searchS.php?ct=0100020003">藝術設計</a></li>
+							<li><a href="searchS.php?ct=0100020004">人文科普</a></li>
+							<li><a href="searchS.php?ct=0100020005">語言電腦</a></li>
+							<li><a href="searchS.php?ct=0100020006">心靈養生</a></li>
+							<li><a href="searchS.php?ct=0100020007">休閒生活</a></li>
+							<li><a href="searchS.php?ct=0100020008">其他書籍</a></li>
+						</ul>
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=010002">所有一般書籍</a>
+					</div>
+				</div>
+				<div id="magazine" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0100030001">新聞時事</a></li>
+							<li><a href="searchS.php?ct=0100030003">語言、文學、史地</a></li>
+							<li><a href="searchS.php?ct=0100030002">財經企管</a></li>
+							<li><a href="searchS.php?ct=0100030004">科學、電腦</a></li>
+							<li><a href="searchS.php?ct=0100030005">藝術、設計、攝影</a></li>
+							<li><a href="searchS.php?ct=0100030006">相機攝影</a></li>
+							<li><a href="searchS.php?ct=0100030007">流行影視</a></li>
+							<li><a href="searchS.php?ct=0100030008">旅遊、休閒、生活</a></li>
+							<li><a href="searchS.php?ct=0100030009">其他雜誌</a></li>
+						</ul>
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=010003">所有報刊雜誌</a>
+					</div>
+				</div>
+				<div id="foreign_book" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0100040001">商業理財</a></li>
+							<li><a href="searchS.php?ct=0100040002">文學小說</a></li>
+							<li><a href="searchS.php?ct=0100040003">藝術設計</a></li>
+							<li><a href="searchS.php?ct=0100040004">人文科普</a></li>
+							<li><a href="searchS.php?ct=0100040005">語言電腦</a></li>
+							<li><a href="searchS.php?ct=0100040006">心靈養生</a></li>
+							<li><a href="searchS.php?ct=0100040007">休閒生活</a></li>
+							<li><a href="searchS.php?ct=0100040008">其他書籍</a></li>
+						</ul>
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=010004">所有外文書籍</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!--書籍雜誌-->
 	
-	<!--a href="#books_and_ magazine">ttt</a-->
+	<hr/>
 	
-	<!--總分類列表-->
-	<div class="">
-		<!--books and magazine-->
-		<ul class="nav nav-tabs">
-			<li><h4><i class="icon-book"></i> <a name="books_and_ magazine" href="#">書籍雜誌</a></h4></li>
-		</ul>
-		<div class="tab-content">
-			<div class="row-fluid">
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5><a href="#">校內課程用書</a></h5></li>
-						<ul>
-						<li><a href="#">文學院</a></li>
-						<li><a href="#">理學院</a></li>
-						<li><a href="#">工學院</a></li>
-						<li><a href="#">管理學院</a></li>
-						<li><a href="#">海洋科學院</a></li>
-						<li><a href="#">社會科學院</a></li>
-						<li><a href="#">通識課程</a></li>
-						<li><a href="#">其他課程</a></li>
-						</ul>
-					</ul> 
-				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5><a href="#">一般書籍</a></h5></li>
-						<ul>
-						<li><a href="#">商業理財</a></li>
-						<li><a href="#">文學小說</a></li>
-						<li><a href="#">藝術設計</a></li>
-						<li><a href="#">人文科普</a></li>
-						<li><a href="#">語言電腦</a></li>
-						<li><a href="#">休閒生活</a></li>
-						<li><a href="#">畫冊漫畫</a></li>
-						<li><a href="#">其他書籍</a></li>
-						</ul>
-					</ul> 
-				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5><a href="#">雜誌期刊</a></h5></li>
-						<ul>
-						<li><a href="#">新聞時事</a></li>
-						<li><a href="#">財經商管</a></li>
-						<li><a href="#">語言、文學、史地</a></li>
-						<li><a href="#">科學、電腦</a></li>
-						<li><a href="#">藝術、設計、攝影</a></li>
-						<li><a href="#">流行時尚、影視偶像</a></li>
-						<li><a href="#">旅遊、休閒、生活</a></li>
-						<li><a href="#">其他雜誌</a></li>
-						</ul>
-					</ul> 
-				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5><a href="#">外文書</a></h5></li>
-						<ul>
-						<li><a href="#">商業理財</a></li>
-						<li><a href="#">文學小說</a></li>
-						<li><a href="#">藝術設計</a></li>
-						<li><a href="#">人文科普</a></li>
-						<li><a href="#">語言電腦</a></li>
-						<li><a href="#">休閒生活</a></li>
-						<li><a href="#">畫冊漫畫</a></li>
-						<li><a href="#">其他書籍</a></li>
-						</ul>
-					</ul> 
-				</div>
-			</div>
-			
+	<!--生活雜物-->	
+	<div class="row-fluid" >
+		<div class="span2">
+			<h4><a href="searchS.php?ct=02">生活雜物</a></h4>
 		</div>
-		
-		
-		<ul class="nav nav-tabs">
-			<li class="active"><span style="text-align:top;line-height:50px; "> <i class="icon-shopping-cart"></i> <strong>服飾精品</strong></span></li>
-		</ul>
-		<div class="tab-content">
-			<div class="row-fluid">
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>女用</h5></li>
-						<ul>
-						<li>服飾</li>
-						<li>包款</li>
-						<li>鞋子</li>
-						<li>配件</li>
-						<li>其他</li>
+		<div class="span10">
+			<ul class="nav nav-tabs" id="trivial">
+				<li class="active"><a data-toggle="tab" href="#clothes">服飾精品</a></li>
+				<li><a data-toggle="tab" href="#beauty_care">美容保養</a></li>
+				<li><a data-toggle="tab" href="#stationery">文具小物</a></li>
+				<li><a data-toggle="tab" href="#3c">3C產品</a></li>
+				<li><a data-toggle="tab" href="#ticket">各式票券</a></li>
+				<li><a data-toggle="tab" href="#house">家具家電</a></li>
+				<li><a data-toggle="tab" href="#traffic">交通工具</a></li>
+				<li><a href="searchS.php?ct=0200080001">食品</a></li>
+				<li><a href="searchS.php?ct=0200090001">其他雜物</a></li>
+				<!--li><a data-toggle="tab" href="#food">食品</a></li>
+				<li><a data-toggle="tab" href="#other">其他雜物</a></li-->
+			</ul>
+			<div class="tab-content" id="trivialSubCat">
+				<div id="clothes" class="tab-pane fade in active">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0200010001">女性時裝</a></li>
+							<li><a href="searchS.php?ct=0200010002">男性時裝</a></li>
+							<li><a href="searchS.php?ct=">其他</a></li>
 						</ul>
-					</ul> 
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=020001">所有服飾精品</a>
+					</div>
 				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>男用</h5></li>
-						<ul>
-						<li>服飾</li>
-						<li>包款</li>
-						<li>鞋子</li>
-						<li>配件</li>
-						<li>其他</li>
+				<div id="beauty_care" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0200020001">保養用品</a></li>
+							<li><a href="searchS.php?ct=0200020002">化妝用品</a></li>
+							<li><a href="searchS.php?ct=0200020003">健身用品</a></li>
+							<li><a href="searchS.php?ct=0200020004">清潔用品</a></li>
+							<li><a href="searchS.php?ct=0200020005">其他</a></li>
 						</ul>
-					</ul> 
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=020002">所有美容保養</a>
+					</div>
 				</div>
-			</div>
-		</div>
-		
-		<ul class="nav nav-tabs">
-			<li class="active"><span style="text-align:top;line-height:50px; "> <i class="icon-book"></i> <strong>生活雜物</strong></span></li>
-		</ul>
-		<div class="tab-content">
-			<div class="row-fluid">
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>生活家電</h5></li>
-						<ul>
-						<li>電冰箱</li>
-						<li>其他</li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
+				<div id="stationery" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0200030001">文具用品</a></li>
+							<li><a href="searchS.php?ct=0200030002">收納整理</a></li>
+							<li><a href="searchS.php?ct=0200030003">其他</a></li>
 						</ul>
-					</ul> 
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=020003">所有文具小物</a>
+					</div>
 				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>電腦及週邊產品</h5></li>
-						<ul>
-						<li>桌電筆電</li>
-						<li>週邊設備</li>
-						<li>電腦零組件</li>
-						<li>軟體</li>
-						<li>網路設備</li>
-						<li>其他</li>
-						<li></li>
-						<li></li>
+				<div id="3c" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0200040001">電腦與周邊</a></li>
+							<li><a href="searchS.php?ct=0200040002">手機與周邊</a></li>
+							<li><a href="searchS.php?ct=0200040003">影音與周邊</a></li>
+							<li><a href="searchS.php?ct=0200040005">相機與周邊</a></li>
+							<li><a href="searchS.php?ct=0200040004">其他</a></li>
 						</ul>
-					</ul> 
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=020004">所有3C產品</a>
+					</div>
 				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>第二層小標題。最多四項</h5></li>
-						<ul>
-						<li>第三層小分類。最多七項+一個其他(盡量)</li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
+				<div id="ticket" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0200050001">交通票券</a></li>
+							<li><a href="searchS.php?ct=0200050002">活動票券</a></li>
+							<li><a href="searchS.php?ct=0200050003">餐券禮券</a></li>
+							<li><a href="searchS.php?ct=0200050004">電影票券</a></li>
+							<li><a href="searchS.php?ct=0200050005">其他</a></li>
 						</ul>
-					</ul> 
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=020005">所有各式票券</a>
+					</div>
 				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>第二層小標題。最多四項</h5></li>
-						<ul>
-						<li>第三層小分類。最多七項+一個其他(盡量)</li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
+				<div id="house" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0200060001">電冰箱</a></li>
+							<li><a href="searchS.php?ct=0200060002">廚房家電</a></li>
+							<li><a href="searchS.php?ct=0200060003">其他家電</a></li>
+							<li><a href="searchS.php?ct=0200060004">衛浴用品</a></li>
+							<li><a href="searchS.php?ct=0200060005">各種家具</a></li>
 						</ul>
-					</ul> 
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=020006">所有家具家電</a>
+					</div>
 				</div>
+				<div id="traffic" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0200070001">腳踏車</a></li>
+							<li><a href="searchS.php?ct=0200050001">交通票券</a></li>
+							<li><a href="searchS.php?ct=0200070002">其他</a></li>
+						</ul>
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=020007">所有交通工具</a>
+					</div>
+				</div>
+				<!--div id="food" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct=0200080001">食品</a></li>
+						</ul>
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=020008">所有食品</a>
+					</div>
+				</div>
+				<div id="other" class="tab-pane fade">
+					<div class="span12">
+						<ul class="vertical_list">
+							<li><a href="searchS.php?ct="></a></li>
+						</ul>
+					</div>
+					<div>
+						<i class="icon-chevron-right"></i> <a href="searchS.php?ct=">所有</a>
+					</div>
+				</div-->
 			</div>
 		</div>
+	</div>
+	<!--生活雜物-->
+	<hr/>
 		
-		<ul class="nav nav-tabs">
-			<li class="active"><span style="text-align:top;line-height:50px; "> <i class="icon-book"></i> <strong>第一層標題</strong></span></li>
-		</ul>
-		<div class="tab-content">
-			<div class="row-fluid">
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>第二層小標題。最多四項</h5></li>
-						<ul>
-						<li>第三層小分類。最多七項+一個其他(盡量)</li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						</ul>
-					</ul> 
-				</div>
-				<div class="span3 well well-small">
-					<ul>
-						<li><h5>第二層小標題。最多四項</h5></li>
-						<ul>
-							<li>第三層小分類。最多七項+一個其他(盡量)</li>
-							<li></li>
-							<li></li>
-							<li></li>
-							<li></li>
-							<li></li>
-							<li></li>
-							<li></li>
-						</ul>
-					</ul> 
-				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>第二層小標題。最多四項</h5></li>
-						<ul>
-						<li>第三層小分類。最多七項+一個其他(盡量)</li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						</ul>
-					</ul> 
-				</div>
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>第二層小標題。最多四項</h5></li>
-						<ul>
-						<li>第三層小分類。最多七項+一個其他(盡量)</li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						</ul>
-					</ul> 
-				</div>
-			</div>
-		</div>
-		
-		<!--ul class="nav nav-tabs">
-			<li class="active"><span style="text-align:top;line-height:50px; "> <i class="icon-book"></i> <strong>第一層標題</strong></span></li>
-		</ul>
-		<div class="tab-content">
-			<div class="row-fluid">
-				<div class="span3 well well-small">
-					<ul>
-					<li><h5>第二層小標題。最多四項</h5></li>
-						<ul>
-						<li>第三層小分類。最多七項+一個其他(盡量)</li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						<li></li>
-						</ul>
-					</ul> 
-				</div>
-			</div>
-		</div-->
-    </div>
+
+
 </div>
 
 
